5736d85d8d17fec7ac50fb582b590ff676c4d468
Fernando Martinez Monguí <luis.martinez@gerleinco.com>
2026-01-05 12:27:01 -0500

    creacion del proyecto

---
+++ src/main/java/pruebaaccenturebackend/fintech/PruebaSoftkaBackendApplication.java
@@ -0,0 +1,13 @@
+package pruebaaccenturebackend.fintech;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PruebaSoftkaBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PruebaSoftkaBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pruebaaccenturebackend/fintech/PruebaSoftkaBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package pruebaaccenturebackend.fintech;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PruebaSoftkaBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
